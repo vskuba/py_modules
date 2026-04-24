0f1127fa7d1864ae80e0327e2a0a90d79bf2aedc
vskuba <vasyl.skuba@protonixltd.com>
2026-04-24 18:13:31 +0300

    update

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,34 +1,21 @@
-import os
+import queue
 
-from redis import Redis
 from rq import Queue
-from typing import Dict, Any
+from typing import Dict
 
-_conn = None
 _queues_cache: Dict[str, Queue] = {}
 
 
-def redis_conn_get():
-    global _conn
-    if _conn is None:
-        redis_url = os.getenv("REDIS_URL", "redis://redis:6379")
-        _conn = Redis.from_url(redis_url)
-    return _conn
-
-
 def queue_init(queues: list[str]):
-    conn = redis_conn_get()
-
     for name in queues:
         if name not in _queues_cache:
-            _queues_cache[name] = Queue(name, connection=conn)
+            _queues_cache[name] = queue.Queue()
 
     return _queues_cache
 
 
 def queue_get(name: str) -> Queue:
     if name not in _queues_cache:
-        conn = redis_conn_get()
-        _queues_cache[name] = Queue(name, connection=conn)
+        _queues_cache[name] = queue.Queue()
 
     return _queues_cache[name]
