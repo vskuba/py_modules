4fabcf0d2dfbd08717d84722c02e35c8072761a4
vskuba <vasyl.skuba@protonixltd.com>
2026-04-24 17:32:33 +0300

    update

--- queue_/queue_.py
+++ queue_/queue_.py
@@ -1,5 +1,23 @@
+import os
+
+from redis import Redis
+from rq import Queue
 from typing import Dict, Any
+
 from state.state import state_get, state_set
+
+
+def queue_init(queues: list[str]):
+    redis_url = os.getenv("REDIS_URL", "redis://redis:6379")
+    conn = Redis.from_url(redis_url)
+    queues_list = {}
+
+    for i in queues:
+        queues_list[i] = Queue(i, connection=conn)
+
+    queue_set(queues_list)
+
+    return queues_list
 
 
 def queue_get() -> Dict[str, Any] | None:
