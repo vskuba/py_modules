4df06703eb0c1b36185d2d085c64d0f95a4ecbab
vskuba <vasyl.skuba@protonixltd.com>
2026-04-26 13:02:29 +0300

    update

--- mysql_/mysql_.py
+++ mysql_/mysql_.py
@@ -1,17 +1,28 @@
 import pymysql
 import pymysql.cursors
+import aiomysql
+
+from typing import Optional
+from aiomysql import Pool
+
+pool: Optional[aiomysql.Pool] = None
+
+host = 'mysql'
+user = 'developer'
+password = 'password'
+db = 'project'
 
 
 def mysql_get_url() -> str:
-    return 'mysql://developer:password@mysql/project'
+    return f'mysql://{user}:{password}@{host}/{db}'
 
 
 def mysql_conn_get() -> pymysql.Connection:
     return pymysql.connect(
-        host='mysql',
-        user='developer',
-        password='password',
-        database='project',
+        host=host,
+        user=user,
+        password=password,
+        database=db,
         cursorclass=pymysql.cursors.DictCursor,
         charset='utf8mb4'  # Рекомендуется для корректной работы с текстом/эмодзи
     )
@@ -23,3 +34,26 @@ def mysql_get_db():
         yield connection
     finally:
         connection.close()
+
+
+async def mysql_get_db_async():
+    p = await mysql_pool_get()
+    async with p.acquire() as conn:
+        yield conn
+
+
+async def mysql_pool_get() -> Pool | None:
+    global pool
+    if not pool:
+        pool = aiomysql.create_pool(
+            host=host,
+            user=user,
+            password=password,
+            db=db,
+            autocommit=True,
+            cursorclass=aiomysql.DictCursor,
+            minsize=5,
+            maxsize=10
+        )
+
+    return pool
